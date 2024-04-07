944782d8ef6e43d1f0002f24999ad44c9549bc34
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-07 21:45:34 +0200

    add url to my landing page

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from landing.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
+    path('', index_view, name="index")
 ]
