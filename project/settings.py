efdaf008836dfc7ac0a8402e8c4497bf6ab2e317
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-10 23:57:04 +0200

    add environ, crispy and email backend

--- project/settings.py
+++ project/settings.py
@@ -11,20 +11,26 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tyi%mid8=j5xnr!7!h(hx9-o*4ovcje-2y-0xw=!dq$s-y+!_*'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_ckeditor_5',
     'django_extensions',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'account',
     'stockit',
     'landing',
@@ -218,3 +226,17 @@ CKEDITOR_5_CONFIGS = {
         }
     }
 }
+
+
+# Crispy
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Emails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gabrieltrouve5@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_PW')
+DEFAULT_FROM_EMAIL = 'PlatForum<gabrieltrouve5@gmail.com>'
