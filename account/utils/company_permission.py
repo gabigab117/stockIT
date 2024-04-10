1d2a37c5102277250ecea4ec48842dd65ab2fdaa
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-11 01:20:07 +0200

    add funcs to manage form validation when a user create a company (with a atomic transaction). And when an user create a company, he has admin company perm

---
+++ account/utils/company_permission.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+from account.models import Company
+
+
+def add_company_admin_permission(user):
+    content_type = ContentType.objects.get_for_model(Company)
+    permissions = Permission.objects.filter(content_type=content_type)
+    for perm in permissions:
+        if perm.codename == "company_admin_status":
+            user.user_permissions.add(perm)
+    return permissions
