fb7a0c151ba673f4a68534f99e8f83222b7a6fa4
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-10 22:33:01 +0200

    render models admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser, UserAddress, CompanyAddress, Company
 
-# Register your models here.
+models = [CustomUser, UserAddress, CompanyAddress, Company]
+for model in models:
+    admin.site.register(model)
