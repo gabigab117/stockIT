1b1f6a76e07d2ee569895f3bf73e3b6bbfe23ff9
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-16 23:50:03 +0200

    add integration test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import pytest
+from account.models import CustomUser
 from landing.models import CompanyPresentation
 
 
 @pytest.fixture
 def presentation():
     return CompanyPresentation.objects.create(title="presentation", text="super texte !")
+
+
+@pytest.fixture
+def user_1():
+    return CustomUser.objects.create_user(email="gabrieltrouve5@gmail.com", username="gabigab117", first_name="Trouvé",
+                                          last_name="Gabriel", password="12345678")
