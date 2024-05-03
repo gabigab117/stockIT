5370f015d4c09d62d1a883f172ee5f4d9ac5aa3b
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-03 02:02:23 +0200

    add supplier form

--- stockit/forms.py
+++ stockit/forms.py
@@ -2,7 +2,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Submit
 from django import forms
 
-from stockit.models import Product
+from stockit.models import Product, Supplier
 
 
 class ProductForm(forms.ModelForm):
@@ -29,5 +29,30 @@ class ProductForm(forms.ModelForm):
                 css_class="form-row"
             ),
             'suppliers',
+            Submit("submit", "Valider")
+        )
+
+
+class SupplierForm(forms.ModelForm):
+    class Meta:
+        model = Supplier
+        exclude = ["identification", "company"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column("name", css_class='form-group col-md-6 mb-0'),
+                Column("phone", css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column("website", css_class='form-group col-md-6 mb-0'),
+                Column("email", css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column("rib", css_class='form-group col-md-6 mb-0'),
+                Column("kbis", css_class='form-group col-md-6 mb-0')
+            ),
             Submit("submit", "Valider")
         )
