379f8e288492e91c66571ef0a71237ba46020327
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-03 02:01:11 +0200

    add media into url

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from landing.views import index_view
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("stockit/", include("stockit.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
