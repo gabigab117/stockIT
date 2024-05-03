18902e664f6c49d752cffebf50510a08cdd6e264
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-03 02:32:44 +0200

    replace de exception

--- account/context_processors.py
+++ account/context_processors.py
@@ -1,9 +1,9 @@
 from account.models import Company
-from project.utils import get_pk_from_session
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def add_session_company_to_context(request):
     try:
-        return {"company_session": Company.objects.get(pk=get_pk_from_session(request.session.get("company")))}
-    except AttributeError:
+        return {"company_session": Company.objects.get(pk=request.session.get("company"))}
+    except ObjectDoesNotExist:
         return {"company_session": None}
