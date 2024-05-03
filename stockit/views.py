397a420e0ccdaf3caea4fc430310ba76212218a8
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-03 02:00:31 +0200

    add create supplier

--- stockit/views.py
+++ stockit/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.decorators import login_required
 
-from .forms import ProductForm
+from account.models import Company
+from .forms import ProductForm, SupplierForm
 from .utils import company_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from project.utils import get_pk_from_session
 
-from stockit.models import Product
+from stockit.models import Product, Supplier
 
 
 @method_decorator(login_required, name="dispatch")
@@ -20,4 +22,17 @@ class CreateArticle(CreateView):
 
     def form_valid(self, form):
         form.instance.company = self.request.session["company"]
+        return super().form_valid(form)
+
+
+@method_decorator(login_required, name="dispatch")
+@method_decorator(company_required, name="dispatch")
+class CreateSupplier(CreateView):
+    model = Supplier
+    form_class = SupplierForm
+    success_url = reverse_lazy("index")
+    template_name = "stockit/create-supplier.html"
+
+    def form_valid(self, form):
+        form.instance.company = Company.objects.get(pk=get_pk_from_session(self.request.session["company"]))
         return super().form_valid(form)
