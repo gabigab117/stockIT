6cf2f6ffa0cc2c7628e774e0506ed359bb5b33b0
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-03 02:03:15 +0200

    update context processor to get the company or return None

--- account/context_processors.py
+++ account/context_processors.py
@@ -1,2 +1,9 @@
+from account.models import Company
+from project.utils import get_pk_from_session
+
+
 def add_session_company_to_context(request):
-    return {"company_session": request.session.get("company", None)}
+    try:
+        return {"company_session": Company.objects.get(pk=get_pk_from_session(request.session.get("company")))}
+    except AttributeError:
+        return {"company_session": None}
